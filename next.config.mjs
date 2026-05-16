a168de8aace12acb5dcc6aac8acd1c19044d41b6
mantupatra23-pixel <mantupatra23@gmail.com>
2026-05-16 21:27:36 +0530

    Feat: Next.js boilerplate complete, deployment to Render

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Ye line mandatory hai static export ke liye
-  images: {
-    unoptimized: true, // Termux/Render static export mein image optimization bypass karne ke liye
-  },
+  output: 'export',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
